6f039a6384d8677dca79c6bca7c9331fb122b170
AngelMcDaniel <105662216+AngelMcDaniel@users.noreply.github.com>
2022-05-21 10:18:54 -0500

    Add files via upload

---
+++ Activity4.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_4
+{
+    public partial class Activity4 : Form
+    {
+        //Angel McDaniel
+        public Activity4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textSeconds.Text == "")
+            {
+                MessageBox.Show("Please enter seconds elapsed.");
+            }
+            else
+            {
+                int seconds = Convert.ToInt32(textSeconds.Text);
+
+                if (seconds >= 86400)
+                {
+                    int days = seconds / 86400;
+                    textResults.Text = days + " days";
+                }
+                else if (seconds >= 3600)
+                {
+                    int hours = seconds / 3600;
+                    textResults.Text = hours + " hours";
+                }
+                else if (seconds >= 60)
+                {
+                    int minutes = seconds / 60;
+                    textResults.Text = minutes + " minutes";
+                }
+                else
+                {
+                    textResults.Text = seconds + " seconds";
+                }
+            }
+        }
+    }
+}
